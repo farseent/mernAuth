9c22ef6c1910aabe5a929b997e9f4dbce74158d5
farseen <farseen247@gmail.com>
2025-06-30 17:19:49 +0530

    optimization

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -66,13 +66,3 @@ export const getUser = async (req, res) => {
     res.status(401).json({ message: "Invalid token" });
   }
 };
-
-export const getMe = async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select("-password"); // Exclude password
-    if (!user) return res.status(404).json({ message: "User not found" });
-    res.json({ user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
